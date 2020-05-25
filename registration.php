90159de70ec3712b4a89672addcca818f6891fc6
Yash jain <55264027+YashJain2@users.noreply.github.com>
2020-05-25 08:42:18 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -36,7 +36,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Simple Registration Form in PHP with Validation | Tutsmake.com</title>
+    <title>Simple Registration Form in PHP</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
